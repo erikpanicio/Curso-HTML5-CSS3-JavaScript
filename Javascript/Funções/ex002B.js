c5fc2fb98c6b230105c8b4576426aeb37626b37c
Erik Panicio <erikpanicio.dev@gmail.com>
2022-10-20 23:10:20 -0300

    Delete Javascript directory

--- Javascript/Funções/ex002B.js
+++
@@ -1,6 +0,0 @@
-function soma(n1=0, n2=0) { /* se eu não passar o valor de n2, por exemplo, o valor a ser considerado será 0 */
-    return n1 + n2
-}
-
-var resultado = soma(5)
-console.log(resultado)
